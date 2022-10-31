3c701d148f31c81fd3bd2e99e29b45fb146dadc8
hyuk <wgilooy97@naver.com>
2022-10-31 11:52:40 +0900

    goodkin

---
+++ src/main/java/com/goodkin/GoodkinApplication.java
@@ -0,0 +1,13 @@
+package com.goodkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodkin/GoodkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
